0c5d0b3fb8a4590094ec16dfa46f70cc8cf95ecd
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-11 10:24:50 +0200

    demoed async setState shallow merge with single object argument to update state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: { firstname: 'Simba', lastname: 'Kuwanda' },
+			company: 'Heartbit Technologies',
+		};
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<header className='App-header'>
 					<img src={logo} className='App-logo' alt='logo' />
 					<p>
-						Edit <code>src/App.js</code> and save to reload.
+						Hi my name is {this.state.name.firstname} {this.state.name.lastname}
+						, I work at {this.state.company}.
 					</p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
+					<button
+						onClick={() => {
+							this.setState({
+								name: { firstname: 'Chichie', lastname: 'Kuwanda' },
+							});
+							console.log(this.state);
+						}}
 					>
-						Learn React
-					</a>
+						Click
+					</button>
 				</header>
 			</div>
 		);

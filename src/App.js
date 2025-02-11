62ddcc696f2eb8fc9ba47039a2750cca9f440228
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-11 16:55:57 +0200

    refactoring code to use more components, created the CardList component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -46,11 +47,12 @@ class App extends Component {
 					placeholder='search monsters'
 					onChange={onSearchChange.bind(this)}
 				/>
-				{filteredMonsters.map((monster) => (
+				{/* {filteredMonsters.map((monster) => (
 					<div key={monster.id}>
 						<h1>{monster.name}</h1>
 					</div>
-				))}
+				))} */}
+				<CardList />
 			</div>
 		);
 	}

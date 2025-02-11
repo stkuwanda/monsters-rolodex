0bc091118bea884edf06b8323c3143530e62bb3b
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-11 10:54:56 +0200

    used the Array.prototype.map method to render a list of items inside h1 components from the state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			name: { firstname: 'Simba', lastname: 'Kuwanda' },
-			company: 'Heartbit Technologies',
+			monsters: [{ name: 'Ogre' }, { name: 'Giant' }, { name: 'Cyclops' }, {name: 'Orc'}],
 		};
 	}
 
 	render() {
 		return (
 			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<p>
-						Hi my name is {this.state.name.firstname} {this.state.name.lastname}
-						, I work at {this.state.company}.
-					</p>
-					<button
-						onClick={() => {
-							this.setState(
-								(state, props) => {
-									return {
-										...state,
-										name: { ...state.name, firstname: 'Chichie' },
-									};
-								},
-								() => console.log(this.state)
-							);
-						}}
-					>
-						Click
-					</button>
-				</header>
+				{this.state.monsters.map((monster) => (
+					<h1>{monster.name}</h1>
+				))}
 			</div>
 		);
 	}

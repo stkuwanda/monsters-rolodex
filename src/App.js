1c5a3a21c02f5818afeff718c232fa04ea970048
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-11 15:40:11 +0200

    refactored code to optimize performance by removing recreation of anonymous method inside onChange listener, minor readability improvements through destructuring properties out of the key objects, used func.bind call to bind class object to out of context onSearchChange call inside onChange listener - alternative is to define onSearchChange as a class property initialized with an arrow function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
 		super();
 		this.state = {
 			monsters: [],
+			searchText: '',
 		};
 	}
 
@@ -13,16 +14,39 @@ class App extends Component {
 		const getUsers = async (url) => {
 			const response = await fetch(url);
 			const result = await response.json();
-			this.setState((state, props) => ({ ...state, monsters: result }), () => console.log(this.state));
-		}
+			this.setState(
+				(state, props) => ({ ...state, monsters: result }),
+				() => console.log(this.state)
+			);
+		};
 
 		getUsers('https://jsonplaceholder.typicode.com/users');
 	}
 
+	onSearchChange(event)  {
+		this.setState(
+			{ searchText: event.target.value.toLowerCase() },
+			() => console.log(this.state)
+		);
+	}
+
 	render() {
+		const { monsters, searchText } = this.state;
+		const { onSearchChange } = this;
+
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchText)
+		);
+
 		return (
 			<div className='App'>
-				{this.state.monsters.map((monster) => (
+				<input
+					className='search-box'
+					type='search'
+					placeholder='search monsters'
+					onChange={onSearchChange.bind(this)}
+				/>
+				{filteredMonsters.map((monster) => (
 					<div key={monster.id}>
 						<h1>{monster.name}</h1>
 					</div>

fb520bec2b077a41528b1698695a38e6d071b001
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-12 17:57:43 +0200

    converted CardList class component to function component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,29 +1,26 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 
-class CardList extends Component {
-	render() {
-		const { monsters } = this.props;
+const CardList = (props) => {
+	const { monsters } = props;
 
-		return (
-			<div className='card-list'>
-				{monsters.map((monster) => {
-					const { id, name, email } = monster;
-					const src = `https://robohash.org/${monster.id}?set=set2&size=180x180`;
+	return (
+		<div className='card-list'>
+			{monsters.map((monster) => {
+				const { id, name, email } = monster;
+				const src = `https://robohash.org/${monster.id}?set=set2&size=180x180`;
 
-					return (
-						<Card
-							key={id}
-							className='card-container'
-							name={name}
-							email={email}
-							src={src}
-						/>
-					);
-				})}
-			</div>
-		);
-	}
-}
+				return (
+					<Card
+						key={id}
+						className='card-container'
+						name={name}
+						email={email}
+						src={src}
+					/>
+				);
+			})}
+		</div>
+	);
+};
 
 export default CardList;
